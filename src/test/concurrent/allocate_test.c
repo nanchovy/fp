c7358ba7cfe4cb3367a1d48117644054fee5bf10
naoyukinagayasu <cyouan927@gmail.com>
2020-09-13 01:40:59 +0900

    a

--- src/test/concurrent/allocate_test.c
+++ src/test/concurrent/allocate_test.c
@@ -11,42 +11,29 @@ int main(int argc, char *argv[])
   KeyValuePair kv;
   int loop_times = 400;
   int max_val = 1000;
-  if (argc > 1)
-  {
+  if (argc > 1) {
     loop_times = atoi(argv[1]);
-    if (loop_times <= 0)
-    {
+    if (loop_times <= 0) {
       printf("invalid argument\n");
       return 1;
     }
     max_val = atoi(argv[2]);
-    if (max_val <= 0)
-    {
+    if (max_val <= 0) {
       printf("invalid argument\n");
       return 1;
     }
-  }
-  else
-  {
+  } else {
        printf("default: loop_times = 40, max_val = 1000\n");
   }
   initAllocator(NULL, "/mnt/nvmm/nagayasu/data", sizeof(PersistentLeafNode) * (loop_times * 2 / (MAX_PAIR / 2) + 1 + sizeof(AllocatorHeader)), 1);
   bpt = newBPTree();
   kv.key = 1;
   kv.value = 1;
-  for (int i = 1; i <= loop_times; i++)
-  {
-    kv.key = i;
-    printf("insert %ld\n", kv.key);
-    if (insert(bpt, kv, 1))
-    {
-      printf("success\n");
-    }
-    else
-    {
-      printf("failure\n");
-    }
+  for (int i = 1; i <= 10; i++) {
+    ppointer new_pleaf_p = pst_mem_allocate(16, 1);
+    printfreelist();
   }
+
   showTree(bpt, 1);
 
   return 0;
