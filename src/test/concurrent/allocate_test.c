a9e16380fcacc1aae7ad0f620bb9f856cf6cd2bb
naoyukinagayasu <cyouan927@gmail.com>
2020-09-21 02:59:24 +0900

    unmapを行わない設定でtreeが作れるかテスト

--- src/test/concurrent/allocate_test.c
+++ src/test/concurrent/allocate_test.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     BPTree *bpt;
     KeyValuePair kv;
     int loop_loop_times = 10;
-    int loop_times = 320;
+    int loop_times = 10000;
     int max_val = 1000;
     struct rusage start, finish;
     struct timeval user_time, sys_time;
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
     sys_time.tv_sec = 0;
     sys_time.tv_usec = 0;
 
-    for (int j = 1; j <= loop_times; j++) {
+    // for (int j = 1; j <= loop_times; j++) {
         initAllocator(NULL, "/mnt/nvmm/nagayasu/data", sizeof(PersistentLeafNode) * (loop_times * 2 / (MAX_PAIR / 2) + 1 + sizeof(AllocatorHeader)), 1);
         bpt = newBPTree();
         kv.key = 1;
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
         // showTree(bpt, 1);
         destroyBPTree(bpt, 1);
         destroyAllocator();
-    }
+    // }
     printf("評価結果\n");
     printf("%ld, %ld, %ld, %ld\n", user_time.tv_sec, user_time.tv_usec, sys_time.tv_sec, sys_time.tv_usec);
     return 0;
