3041b57ea70161d42b126a2b11ce9f526d6a87a6
naoyukinagayasu <cyouan927@gmail.com>
2020-09-21 23:54:10 +0900

    allocate_testのパラメータのいくつかを評価用に修正

--- src/test/concurrent/allocate_test.c
+++ src/test/concurrent/allocate_test.c
@@ -6,12 +6,15 @@
 #include <time.h>
 #include <sys/resource.h>
 
+#define ALLOCATING_BYTES 320
+
 int main(int argc, char *argv[])
 {
   struct rusage start, finish;
   BPTree *bpt;
   KeyValuePair kv;
-  int loop_times = 4;
+  
+  int loop_times = 100;
   int max_val = 1000;
   if (argc > 1) {
     loop_times = atoi(argv[1]);
@@ -27,14 +30,14 @@ int main(int argc, char *argv[])
   } else {
        printf("default: loop_times = 40, max_val = 1000\n");
   }
-  initAllocator(NULL, "/mnt/nvmm/nagayasu/data", sizeof(PersistentLeafNode) * (loop_times * 2 / (MAX_PAIR / 2) + 1 + sizeof(AllocatorHeader)), 1);
+  initAllocator(NULL, "/mnt/nvmm/nagayasu/data", 320 * 150, 1);
   bpt = newBPTree();
   kv.key = 1;
   kv.value = 1;
 
   getrusage(RUSAGE_SELF, &start);
-  for (int i = 1; i <= 10; i++) {
-    ppointer new_pleaf_p1 = pst_mem_allocate(16, 1);
+  for (int i = 1; i <= loop_times; i++) {
+    ppointer new_pleaf_p1 = pst_mem_allocate(ALLOCATING_BYTES, 1);
     printfreelist();
   }
   getrusage(RUSAGE_SELF, &finish);
