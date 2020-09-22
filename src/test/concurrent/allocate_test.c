a07cfbebc190430d595b655642ab8005099607dc
naoyukinagayasu <cyouan927@gmail.com>
2020-09-22 23:46:35 +0900

    より厳密に時間を図るように修正

--- src/test/concurrent/allocate_test.c
+++ src/test/concurrent/allocate_test.c
@@ -10,7 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-  struct rusage start, finish;
+  struct rusage start, finish, result;
+  result.ru_utime.tv_sec = result.ru_utime.tv_ssec = 0;
+  result.ru_stime.tv_usec = result.ru_stime.tv_usec = 0;
+
   BPTree *bpt;
   KeyValuePair kv;
   
@@ -22,19 +25,25 @@ int main(int argc, char *argv[])
   kv.key = 1;
   kv.value = 1;
 
-  getrusage(RUSAGE_SELF, &start);
+  
   for (int i = 1; i <= loop_times; i++) {
+    getrusage(RUSAGE_SELF, &start);
     ppointer new_pleaf_p1 = pst_mem_allocate(ALLOCATING_BYTES, 1);
+    getrusage(RUSAGE_SELF, &finish);
+    result.ru_utime.tv_sec += finish.ru_utime.tv_sec - start.ru_utime.tv_sec;
+    result.ru_utime.tv_usec += finish.ru_utime.tv_usec - start.ru_utime.tv_usec;
+    result.ru_stime.tv_sec += finish.ru_stime.tv_sec - start.ru_stime.tv_sec;
+    result.ru_stime.tv_usec += finish.ru_stime.tv_usec - start.ru_stime.tv_usec;
   }
-  getrusage(RUSAGE_SELF, &finish);
+
 //   showTree(bpt, 1);
 
   destroyBPTree(bpt, 1);
   destroyAllocator();
 
 
-  printf("usr time: %ld.%d\n", finish.ru_utime.tv_sec - start.ru_utime.tv_sec, finish.ru_utime.tv_usec - start.ru_utime.tv_usec);
-  printf("sys time: %ld.%d\n", finish.ru_stime.tv_sec - start.ru_stime.tv_sec, finish.ru_stime.tv_usec - start.ru_stime.tv_usec);
+  printf("usr time: %ld.%06d\n", finish.ru_utime.tv_sec - start.ru_utime.tv_sec, finish.ru_utime.tv_usec - start.ru_utime.tv_usec);
+  printf("sys time: %ld.%06d\n", finish.ru_stime.tv_sec - start.ru_stime.tv_sec, finish.ru_stime.tv_usec - start.ru_stime.tv_usec);
 
   return 0;
 }
