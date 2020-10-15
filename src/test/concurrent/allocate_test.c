15d2a3d10a43e515393910cf6acd196ed72fa602
nagayasu <n.ngys@uec.ac.jp>
2020-10-15 04:29:41 +0000

    細かな修正

--- src/test/concurrent/allocate_test.c
+++ src/test/concurrent/allocate_test.c
@@ -6,18 +6,16 @@
 #include <time.h>
 #include <sys/resource.h>
 
-#define ALLOCATING_BYTES 320
+#define ALLOCATING_BYTES 576
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   struct rusage start, finish, result;
   struct timeval total;
   result.ru_utime.tv_sec = result.ru_utime.tv_sec = total.tv_sec = 0;
   result.ru_stime.tv_sec = result.ru_stime.tv_usec = total.tv_usec = 0;
 
-
-  int loop_loop_times = 100;
-  int loop_times = 40;
+  int loop_loop_times = 200000;
+  int loop_times = 50;
 
   // allocatorとMemoryRootの初期化(一回目)
   initAllocator(NULL, "/mnt/nvmm/nagayasu/data", ALLOCATING_BYTES * (loop_times + 5) + 100, 1);
@@ -27,11 +25,10 @@ int main(int argc, char *argv[])
 
     for (int i = 1; i <= loop_times; i++) {
 
-      printf("i, j: %d, %d\n", i, j);
       // pst_mem_allocate関数の実行にかかる時間の測定
       getrusage(RUSAGE_SELF, &start);
       void* new_pleaf_p1 = karmalloc(ALLOCATING_BYTES);
-      printfreelist();
+      // printfreelist();
       getrusage(RUSAGE_SELF, &finish);
       result.ru_utime.tv_sec += finish.ru_utime.tv_sec - start.ru_utime.tv_sec;
       result.ru_stime.tv_sec += finish.ru_stime.tv_sec - start.ru_stime.tv_sec;
@@ -64,15 +61,15 @@ int main(int argc, char *argv[])
   result.ru_stime.tv_sec += result.ru_stime.tv_usec / 1000000;
   result.ru_utime.tv_usec %= 1000000;
   result.ru_stime.tv_usec %= 1000000;
-  printf("usr time: %ld.%ld\n", result.ru_utime.tv_sec, result.ru_utime.tv_usec);
-  printf("sys time: %ld.%ld\n", result.ru_stime.tv_sec, result.ru_stime.tv_usec);
+  printf("%ld.%06ld, ", result.ru_utime.tv_sec, result.ru_utime.tv_usec);
+  printf("%ld.%06ld, ", result.ru_stime.tv_sec, result.ru_stime.tv_usec);
 
   // 合計時間の出力
   total.tv_sec = result.ru_utime.tv_sec + result.ru_stime.tv_sec;
   total.tv_usec = result.ru_utime.tv_usec + result.ru_stime.tv_usec;
   total.tv_sec += total.tv_usec / 1000000;
-  total.tv_usec % 1000000;
-  printf("total time: %ld.%ld\n", total.tv_sec, total.tv_usec);
+  total.tv_usec %= 1000000;
+  printf("%ld.%06ld\n", total.tv_sec, total.tv_usec);
 
   return 0;
 }
